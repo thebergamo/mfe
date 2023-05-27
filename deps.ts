b5aa10d52f094e95e5b3def1e09beeb206af6e59
Marcos Bérgamo <marcos@thedon.com.br>
2023-05-27 16:45:50 +0200

    chore: use import maps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
-import React from "https://esm.sh/react@18.2.0&dev";
+import React from "https://esm.sh/react@next&dev";
 
 export { Hono } from "https://deno.land/x/hono@v3.0.2/mod.ts";
 export { HonoRequest } from "https://deno.land/x/hono@v3.0.2/request.ts";
@@ -16,15 +16,15 @@ export { getLogger } from "https://deno.land/std@0.177.0/log/mod.ts";
 export * as log from "https://deno.land/std@0.177.0/log/mod.ts";
 
 export { React };
-export { renderToReadableStream } from "https://esm.sh/react-dom@18.2.0&dev/server";
-export type { ReactDOMServerReadableStream } from "https://esm.sh/react-dom@18.2.0&dev/server";
+export { renderToReadableStream } from "https://esm.sh/react-dom@next&dev/server";
+export type { ReactDOMServerReadableStream } from "https://esm.sh/react-dom@next&dev/server";
 export {
   createStaticHandler,
   createStaticRouter,
   StaticRouterProvider,
-} from "https://esm.sh/react-router-dom@6.10.0&dev/server";
+} from "https://esm.sh/react-router-dom@6.10.0&dev/server?deps=react@next";
 
-export type { StaticHandlerContext } from "https://esm.sh/react-router-dom@6.10.0&dev/server";
-export type { RouteObject } from "https://esm.sh/react-router-dom@6.10.0/dist/index.d.ts";
+export type { StaticHandlerContext } from "https://esm.sh/react-router-dom@6.10.0&dev/server?deps=react@next";
+export type { RouteObject } from "https://esm.sh/react-router-dom@6.10.0/dist/index.d.ts?deps=react@next";
 export * as twind from "https://esm.sh/twind@0.16.19";
 export * as twindSheets from "https://esm.sh/twind@0.16.19/sheets";
