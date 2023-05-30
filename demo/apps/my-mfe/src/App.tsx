efef031831d49c5fbf84536e78fb5fc90fa09870
Marcos Bérgamo <marcos@thedon.com.br>
2023-05-30 22:20:40 +0200

    chore: remove extra demo app

--- demo/apps/my-mfe/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { routes } from './routes'
-
-let router = createBrowserRouter(routes, { basename: '/mfe-1' })
-function App() {
-  return <RouterProvider router={router} />
-}
-
-export default App
--- demo/apps/my-mfe/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  presets: [require('mfe-ui/tailwind.config.cjs')],
-  corePlugins: {
-    preflight: true
-  },
-}
